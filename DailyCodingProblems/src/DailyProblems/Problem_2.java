7dea9e58e16afeec596099acf7bd7e807d84010c
Chaitra <6chaitra8@gmail.com>
2022-05-02 22:40:29 +0530

    Initail Commit

---
+++ DailyCodingProblems/src/DailyProblems/Problem_2.java
@@ -0,0 +1,73 @@
+package DailyProblems;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Problem_2 {
+
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the length of the array: ");
+        int size = sc.nextInt();
+
+        int[] array = new int[size];
+
+        System.out.println("Enter the Array Elements : ");
+
+        for(int i = 0 ; i < size; i++){
+            array[i] = sc.nextInt();
+        }
+
+
+        int[] result = arrayProduct(array, size);
+
+        for(int i = 0; i<size ; i++){
+            System.out.println(result[i] + "   ");
+
+        }
+
+
+
+    }
+
+    private static int[] arrayProduct(int[] array, int size) {
+
+        if (size == 1) {
+            System.out.println("Array has only one element...\nArray Product cannot be formed");
+            return array;
+        }
+
+        int[] leftArr = new int[size];
+        int[] rightArr = new int[size];
+        int[] productArr = new int[size];
+
+        leftArr[0] = 1;
+        rightArr[size-1] = 1;
+
+
+        // Generate left array
+        for(int i = 1; i< size; i++){
+            leftArr[i]= array[i-1] * leftArr[i-1];
+        }
+
+
+        //Generate right array
+        for(int i = size-2; i >=0 ; i--){
+            rightArr[i]= array[i+1] * rightArr[i+1];
+        }
+
+
+        // generating product array
+        for(int i = 0; i<size ; i++){
+
+            productArr[i] = leftArr[i] * rightArr[i];
+        }
+
+        return productArr;
+
+    }
+
+}
